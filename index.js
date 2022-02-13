e2d2aa530854511e12e74a636d6fde6b520fd5a9
Samantha Manning <sammanning13@gmail.com>
2022-02-12 21:00:00 -0500

    variables

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-       // Comments explain why and how code is written not 
-       console.log('hello world');
+// Comments explain why and how code is written not 
+console.log('hello world')
+
+//Variables:
+let name = 'Mosh';
+console.log(name);
+//Cannot be reserved keyword
+//Should be meaningful and descriptive (what is the purpose, what type of data is stored)
+//Cannot start with a number (1name)
+//Cannot contain a space or hyphen (-)
+//Are case-sensitive
+
+//multiple variables
+let firstName = 'Mosh';
+let lastName = 'Hamedani';      
+
+//Constant variables are immutable; this will throw an error
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
