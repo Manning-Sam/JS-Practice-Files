66ae693c03894c003051115f822d250a90ac7676
Samantha Manning <sammanning13@gmail.com>
2022-02-14 19:35:02 -0500

    Equality Operators

--- operators.js
+++ operators.js
@@ -1,12 +1,12 @@
-//Arithmatic
+// Arithmatic Operators
 let x = 10;
 let y = 3;
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y);
-// console.log(x ** y);
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
 
 // Increment (++)
 console.log(++x);
@@ -14,3 +14,23 @@ console.log(x++);
 // Decrement (--)
 console.log(--x);
 console.log(x--);
+
+// Assignment Operators
+// arithmetic operator followed by = ex:(+=)
+x = x+5
+x += 5
+// Comparison Operators
+// results of comparison operators are boolean (true/false)
+// Relational Operators
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+// Equality
+console.log(x === 1);
+console.log(x !== 1);
+
+// Equlity Operators
+console.log('1' === 1); //strict equality checks for same type AND value
+console.log(1 == 1); //lose equality converts value's types on right to type of left value then checks for equality
+
